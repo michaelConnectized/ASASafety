17aff573a9781ce7d683d711cb50bc7a8b664583
michaelConnectized <michael@connectized.com>
2019-06-18 13:20:30 +0800

    backup

---
+++ app/src/main/java/com/asa/asasafety/Object/DangerZoneCondition.java
@@ -0,0 +1,63 @@
+package com.asa.asasafety.Object;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DangerZoneCondition {
+    private String hubid;
+    private String op;
+    private int rssi;
+
+    public static DangerZoneCondition GetDangerZoneConditionFromJson(String json) throws JSONException {
+        DangerZoneCondition resultDangerZoneCondition = new DangerZoneCondition();
+        JSONObject jsonObject = new JSONObject(json);
+        resultDangerZoneCondition.setHubid(jsonObject.getString("hubid"));
+        resultDangerZoneCondition.setOp(jsonObject.getString("op"));
+        resultDangerZoneCondition.setRssi(jsonObject.getInt("rssi"));
+        return resultDangerZoneCondition;
+    }
+
+    public DangerZoneCondition() {}
+
+    public DangerZoneCondition(String hubid, String op, int rssi) {
+        this.hubid = hubid;
+        this.op = op;
+        this.rssi = rssi;
+    }
+
+    public String getHubid() {
+        return hubid;
+    }
+
+    public String getOp() {
+        return op;
+    }
+
+    public int getRssi() {
+        return rssi;
+    }
+
+    public void setHubid(String hubid) {
+        this.hubid = hubid;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public void setRssi(int rssi) {
+        this.rssi = rssi;
+    }
+
+    public String toString() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("hubid", hubid);
+            jsonObject.put("op", op);
+            jsonObject.put("rssi", rssi);
+        } catch (JSONException e) {
+            return "";
+        }
+        return jsonObject.toString();
+    }
+}
